31fa802d68a02839b6165e535fb5634c31e23e2a
heraxusitory <vladiksmart@gmail.com>
2020-10-01 18:53:47 +0300

    Добавлен confirm

--- 2.js
+++ 2.js
@@ -1,32 +1,38 @@
-var firstNumber = prompt('Введите первое число!');
-if (firstNumber != null) {
-	while ( firstNumber === '' || !(firstNumber/1)) {
-	 	if (firstNumber === '0' || firstNumber == null) {
-	 		break;
-	 	}
-	 	firstNumber = prompt('Что-то пошло не так! Введите первое число! Оно должно состоять из цифр!');
-	 }
-if (firstNumber != null) {
-	var secondNumber = prompt('Введите второе число!');
-	while ( secondNumber === ''  || !(secondNumber/1)) {
-	 	if (secondNumber === '0' ||  secondNumber == null) {
-	 		break;
-	 	}
-	 	secondNumber = prompt('Что-то пошло не так! Введите второе число! Оно должно состоять из цифр!');
-	 }
-if (secondNumber != null) {
-	if (firstNumber > secondNumber) {
-		alert('Первое число ' + firstNumber +
-		 ' больше второго числа ' + secondNumber);
+var isEnd = true;
+while (isEnd == true) {
+	var firstNumber = prompt('Введите первое число!');
+
+	if (firstNumber != null) {
+		while ( firstNumber === '' || !(firstNumber/1)) {
+		 	if (firstNumber === '0' || firstNumber == null) {
+		 		break;
+		 	}
+			firstNumber = prompt('Что-то пошло не так! Введите первое число! Оно должно состоять из цифр!');
+		 }
+		if (firstNumber != null) {
+			var secondNumber = prompt('Введите второе число!');
+			while ( secondNumber === ''  || !(secondNumber/1)) {
+			 	if (secondNumber === '0' ||  secondNumber == null) {
+			 		break;
+			 	}
+			 	secondNumber = prompt('Что-то пошло не так! Введите второе число! Оно должно состоять из цифр!');
+			 }
+			if (secondNumber != null) {
+				if (firstNumber > secondNumber) {
+					alert('Первое число ' + firstNumber.trim() +
+					 ' больше второго числа ' + secondNumber.trim());
+				}
+				if (firstNumber < secondNumber) {
+					alert('Первое число ' + firstNumber.trim() +
+					 ' меньше второго числа ' + secondNumber.trim());
+				}
+				if (firstNumber == secondNumber) {
+					alert('Первое число ' + firstNumber +
+					 ' равно второму числу ' + secondNumber.trim());
+				}
+			}
+		}
 	}
-	if (firstNumber < secondNumber) {
-		alert('Первое число ' + firstNumber +
-		 ' меньше второго числа ' + secondNumber);
-	}
-	if (firstNumber == secondNumber) {
-		alert('Первое число ' + firstNumber +
-		 ' равно второму числу ' + secondNumber);
-	}
-}
-}
+	
+isEnd = confirm("Запустить еще раз?");
 }
