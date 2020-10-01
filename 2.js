9c3fde9a3d61b85b59244563d80dea3b78b1a726
heraxusitory <vladiksmart@gmail.com>
2020-10-01 18:29:35 +0300

    Сделаны улучшения, теперь можно нажать кнопку отмены в любом выводе alert

--- 2.js
+++ 2.js
@@ -1,28 +1,32 @@
 var firstNumber = prompt('Введите первое число!');
-while ( firstNumber === '' || firstNumber == null 
- 	|| !(firstNumber/1)) {
- 	if (firstNumber === '0') {
- 		break;
- 	}
- 	firstNumber = prompt('Что-то пошло не так! Введите первое число! Оно должно состоять из цифр!');
- }
-var secondNumber = prompt('Введите второе число!');
-while ( secondNumber === '' || secondNumber == null 
- 	|| !(secondNumber/1)) {
- 	if (secondNumber === '0') {
- 		break;
- 	}
- 	secondNumber = prompt('Что-то пошло не так! Введите второе число! Оно должно состоять из цифр!');
- }
-if (firstNumber > secondNumber) {
-	alert('Первое число ' + firstNumber +
-	 ' больше второго числа ' + secondNumber);
+if (firstNumber != null) {
+	while ( firstNumber === '' || !(firstNumber/1)) {
+	 	if (firstNumber === '0' || firstNumber == null) {
+	 		break;
+	 	}
+	 	firstNumber = prompt('Что-то пошло не так! Введите первое число! Оно должно состоять из цифр!');
+	 }
+if (firstNumber != null) {
+	var secondNumber = prompt('Введите второе число!');
+	while ( secondNumber === ''  || !(secondNumber/1)) {
+	 	if (secondNumber === '0' ||  secondNumber == null) {
+	 		break;
+	 	}
+	 	secondNumber = prompt('Что-то пошло не так! Введите второе число! Оно должно состоять из цифр!');
+	 }
+if (secondNumber != null) {
+	if (firstNumber > secondNumber) {
+		alert('Первое число ' + firstNumber +
+		 ' больше второго числа ' + secondNumber);
+	}
+	if (firstNumber < secondNumber) {
+		alert('Первое число ' + firstNumber +
+		 ' меньше второго числа ' + secondNumber);
+	}
+	if (firstNumber == secondNumber) {
+		alert('Первое число ' + firstNumber +
+		 ' равно второму числу ' + secondNumber);
+	}
 }
-if (firstNumber < secondNumber) {
-	alert('Первое число ' + firstNumber +
-	 ' меньше второго числа ' + secondNumber);
 }
-if (firstNumber == secondNumber) {
-	alert('Первое число ' + firstNumber +
-	 ' равно второму числу ' + secondNumber);
 }
